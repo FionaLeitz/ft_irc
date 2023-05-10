b2bc0657c598274aa3d04c39deb638283b39a01e
Chine MESTON <cmeston@e2r4p2.clusters.42paris.fr>
2023-05-10 12:53:08 +0200

    magnifique mqkegile

--- Makefile
+++ Makefile
@@ -11,6 +11,7 @@ SRCDIR	= ./src/
 INCDIR	= ./includes/
 
 all: $(OBJ) $(NAME)
+	@bash ./dessin.sh
 
 %.o: %.cpp
 	$(CCXX) $(CXXFLAGS) -o $@ -c $<
---
+++ dessin.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# This is a minimal set of ANSI/VT100 color codes
+_END=$'\x1b[0m'
+_BOLD=$'\x1b[1m'
+_UNDER=$'\x1b[4m'
+_REV=$'\x1b[7m'
+
+# Colors
+_GREY=$'\x1b[30m'
+_RED=$'\x1b[31m'
+_GREEN=$'\x1b[32m'
+_YELLOW=$'\x1b[33m'
+_BLUE=$'\x1b[34m'
+_PURPLE=$'\x1b[35m'
+_CYAN=$'\x1b[36m'
+_WHITE=$'\x1b[37m'
+
+# Inverted, i.e. colored backgrounds
+_IGREY=$'\x1b[40m'
+_IRED=$'\x1b[41m'
+_IGREEN=$'\x1b[42m'
+_IYELLOW=$'\x1b[43m'
+_IBLUE=$'\x1b[44m'
+_IPURPLE=$'\x1b[45m'
+_ICYAN=$'\x1b[46m'
+_IWHITE=$'\x1b[47m'
+
+while IFS= read -r line
+do
+	prefix=$(echo "$line" | cut -c 1-20)
+	red=$(echo "$line" | cut -c 21-41)
+	suffix=$(echo "$line" | cut -c 42-)
+	echo -n "$prefix"
+	echo -n "${_RED} $red ${_END}";
+	echo -e "$suffix"
+done < dessin
