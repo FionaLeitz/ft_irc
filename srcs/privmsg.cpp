6c916d5e373794ada4cb6bea1e472f55c04dfad6
Chine Meston <chine@macbook-de-chine.home>
2023-05-19 12:46:32 +0200

    petit pb private message

--- srcs/privmsg.cpp
+++ srcs/privmsg.cpp
@@ -4,13 +4,16 @@ void	ft_privmsg(t_context *context, Client *tmp, struct pollfd *fds, int i, std:
 {
 	(void)context;
 	std::cout << "Received command PRIVMSG w args " << args[0] << " and " << args[1] << std::endl;
+	std::cout << "	client buff is  " << (*tmp).getBuffer() << std::endl;
 	std::string dest;
 	std::string	message;
 	std::string	response;
 
 	dest = args[0];
 	message = args[1];
-	message = message.substr(1, message.size() - 1); // retire le ':' au debut du message
+	message = (*tmp).getBuffer();
+	message = message.substr(message.find(":") + 1);
+	// message = message.substr(1, message.size() - 1); // retire le ':' au debut du message
 	std::cout << "\tmessage = " << message << "\n\tdestinataire = " << dest << std::endl;
 	response = ":" + (*tmp).getNickname() + " PRIVMSG " + dest + " :" + message +"\r\n";
 	send(fds[i + 1].fd, response.c_str(), response.length(), 0);
