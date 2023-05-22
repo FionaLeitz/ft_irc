41a1f1b2e16cd12c44cb25e60e479f9eb411a803
Fiona LEITZ <fleitz@e2r8p3.clusters.42paris.fr>
2023-05-22 11:14:14 +0200

    quit avance, mais on s'appelle quit...

--- srcs/pass.cpp
+++ srcs/pass.cpp
@@ -4,14 +4,18 @@ void	ft_pass(t_context *context, Client *tmp, struct pollfd *fds, int i, std::st
 {
 	std::cout << "Received command PASS w args " << args[0] << " and " << args[1] << std::endl;
 	std::string	response;
+	(void)tmp;
+	(void)fds;
+	(void)i;
+	(void)context;
 
-	if (atoi(args[0].c_str()) == context->password)
-	{
-		std::cout << "Password is valid" << std::endl;
-		// response = RPL_WELCOME((*tmp).getNickname(), (*tmp).getUsername());
-		// send(fds[i].fd, response.c_str(), response.length(), 0);
-	}
-	else
-	{}
+	// if (atoi(args[0].c_str()) == context->password)
+	// {
+	// 	std::cout << "Password is valid" << std::endl;
+	// 	// response = RPL_WELCOME((*tmp).getNickname(), (*tmp).getUsername());
+	// 	// send(fds[i].fd, response.c_str(), response.length(), 0);
+	// }
+	// else
+	// {}
 
 }
