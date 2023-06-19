b4723cb95cf7eb769361a94426b238e98df1c03f
Chine MESTON <cmeston@paul-f3Br4s2.clusters.42paris.fr>
2023-06-19 14:27:33 +0200

    test em cours f notes

--- srcs/who.cpp
+++ srcs/who.cpp
@@ -6,6 +6,12 @@ void	ft_who(t_context *context, Client *tmp, struct pollfd *fds, int i, std::vec
 	std::string	response;
 	std::string nick;
 
+	if (args.size() == 0)
+	{
+		response = ERR_NEEDMOREPARAMS(tmp->getNickname(), "", "WHO");
+		send(fds[i].fd, response.c_str(), response.length(), 0);
+		return ;
+	}
 	nick = (*tmp).getNickname();
 	for (std::map<std::string, Client>::const_iterator it = context->channels[args[0]].getClientlist().begin(); it != context->channels[args[0]].getClientlist().end(); ++it) {
 		// response = ":server 352 " + nick + " " + args[0] + " " + (*it).second.getUsername() + " " + (*it).second.getHost() + " server " + (*it).second.getNickname() + " H :0 " + "realname" + "\r\n";
