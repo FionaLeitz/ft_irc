0ccac2034aa5494a58ecdc268a5671e873779fe8
Fiona LEITZ <fleitz@paul-f3Ar4s10.clusters.42paris.fr>
2023-06-20 13:22:46 +0200

    names et join en cours de patch

--- srcs/names.cpp
+++ srcs/names.cpp
@@ -9,7 +9,7 @@ void	ft_names(t_context *context, Client *tmp, struct pollfd *fds, int i, std::v
 
 	// names prend forcement un argument ?
 	nick = (*tmp).getNickname();
-	for (std::map<std::string, Client>::const_iterator it = context->channels[args[0]].getClientlist().begin(); it != context->channels[args[0]].getClientlist().end(); ++it) {
+	for (std::map<std::string, Client>::const_iterator it = context->channels[args[0]].getClientlist().begin(); it != context->channels[args[0]].getClientlist().end(); it++) {
 		response = RPL_NAMREPLY(nick, "=", args[0], "", it->second.getNickname());
 		std::cout << response << std::endl;
 		send(tmp->getFd(), response.c_str(), response.length(), 0);
